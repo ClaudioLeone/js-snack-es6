74e09ad6853a91779e1acf2570e30f469ea803da
ClaudioLeone <claleone92@gmail.com>
2023-03-24 18:23:46 +0100

    Added 'container' to html doc, made the program find the lightest bike in the bike array and printed on the log

--- Snack_1/script.js
+++ Snack_1/script.js
@@ -1,6 +1,36 @@
 // SNACK 1 //
-// Creare un array di oggetti
-// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+// [X] Creare un array di oggetti
+// [X] Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// [] Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 
-const bicycles
+const bikesArray = [
+    {
+        name: "Atala",
+        weight: 7
+    },
+    {
+        name: "Graziella",
+        weight: 14
+    },
+    {
+        name: "BMX",
+        weight: 5
+    },
+    {
+        name: "Bianchi",
+        weight: 10
+    }
+];
+
+const [bike1, bike2, bike3, bike4] = bikesArray;
+
+bikesArray.forEach(bike => {
+    lighterWgtBike = Math.min(bike1.weight, bike2.weight, bike3.weight, bike4.weight);
+    bikeWgt = bike.weight;
+    
+    if (bikeWgt === lighterWgtBike) {
+        console.log(bike);
+    }
+});
+
+console.log(lighterWgtBike);
