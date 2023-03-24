c2da1d6e4067d8d32f3bee8546af8decd4588369
ClaudioLeone <claleone92@gmail.com>
2023-03-24 19:17:57 +0100

    Working on 'Snack 2'

--- Snack_1/script.js
+++ Snack_1/script.js
@@ -1,7 +1,7 @@
 // SNACK 1 //
 // [X] Creare un array di oggetti
 // [X] Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-// [] Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+// [X] Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 
 const bikesArray = [
     {
--- Snack_2/script.js
+++ Snack_2/script.js
@@ -1,6 +1,38 @@
 // SNACK 2 //
-// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-// Generare numeri random al posto degli 0 nelle proprietà:
-// Punti fatti e falli subiti.
-// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+// [X] Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// [X] Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// [X] Generare numeri random al posto degli 0 nelle proprietà:
+//      - Punti fatti e falli subiti.
+// [] Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const teamsArray = [
+    {
+        name: "Foolean FC",
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: "Boolean FC",
+        points: 0,
+        fouls: 0
+    },
+    {
+        name: "Coolean FC",
+        points: 0,
+        fouls: 0
+    },
+];
+
+
+let updatedTeamArray = [];
+teamsArray.forEach(team => {
+    let teamName = team.name;
+    let teamPoints = team.points;
+    let teamFouls = team.fouls;
+
+    teamPoints = Math.floor(Math.random() * 101);
+    teamFouls = Math.floor(Math.random() * 12);
+
+    console.log("Points are: " + teamPoints);
+    console.log("Fouls are: " + teamFouls);
+});
