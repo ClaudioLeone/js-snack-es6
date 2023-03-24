4b426c2ed03330846f832261b64d472f6ea7e94e
ClaudioLeone <claleone92@gmail.com>
2023-03-24 16:16:02 +0100

    Written guidelines

--- Snack_1/script.js
+++ Snack_1/script.js
@@ -1,1 +1,6 @@
 // SNACK 1 //
+// Creare un array di oggetti
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+const bicycles
